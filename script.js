f2fb5b9a914594cce2978b1c3d1835df4c13fff8
PrinceCharm12 <127068041+PrinceCharm12@users.noreply.github.com>
2024-06-18 14:15:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = 'YEHEEEEEYYYYYYYY THANKS JOCELLE';
+    document.getElementById('messageContainer').innerHTML = 'YEHEEEEEYYYYYYYY THANKS JECELLE';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
